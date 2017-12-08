1d1ab2e4c1c40a1985353d0756e2c9f5c40091ed
JP Dillingham <jpdillingham@gmail.com>
2017-12-08 17:13:28 -0600

    Removed Crypto component

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
-import Crypto from './crypto.js';
 import Item from './item.js';
 
 class App extends Component {
